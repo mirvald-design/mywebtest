59cf09f2ce8d17b0a4232b5fc954abbe1315a0ee
mirvald <mirvald@mirvalds-MacBook-Air.local>
2024-02-26 10:58:10 +0200

    new

--- spotlight-ts/src/app/page.tsx
+++ spotlight-ts/src/app/page.tsx
@@ -273,10 +273,10 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Software designer, founder, and amateur astronaut.
+            Product, founder, and amateur astronaut., founder, and amateur astronaut.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
+            I’m Vadym, a software designer and entrepreneur based in New York
             City. I’m the founder and CEO of Planetaria, where we develop
             technologies that empower regular people to explore space on their
             own terms.
